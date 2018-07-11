a3b830ed1d11487cb63b01dfc6bf2cbffb247cf5
Jorge Casariego <jorgekasa@gmail.com>
2018-07-11 15:15:55 -0400

    12. Present Photo Selector

---
+++ Intagram/AddPhoto/PhotSelectorController.swift
@@ -0,0 +1,93 @@
+//
+//  PhotSelectorController.swift
+//  Intagram
+//
+//  Created by Jorge Casariego on 29/6/18.
+//  Copyright © 2018 Jorge Casariego. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    let cellId = "cellId"
+    let headerId = "headerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .yellow
+        
+        setupNavigationButtons()
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        // Registramos el header que usaremos para mostrar la foto principal
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        // Con esto hacemos que tengamos un pixel de espacio entre el header y las celdas.
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    // Este metodo debemos llamar para que nuestro header sea llamado y mostrado. Sino no muestra
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        // Vamos a hacer que nuestro header sea un cuadrado perfecto!
+        let width = view.frame.width
+        return CGSize(width: width, height: width)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        
+        header.backgroundColor = .red
+        
+        return header
+    }
+    
+    // Este metodo reduce el espacio entre cada celda. Si sacamos esto vamos a ver que hay un espacio muy grande
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    // Lo mismo que minimumLineSpacingForSectionAt. Este metodo usamos para reducir el espacio vertical
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // hacemos -3 para que aparezcan las 3 lineas verticales
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .blue
+        
+        return cell
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func setupNavigationButtons() {
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
+    }
+    
+    @objc func handleNext() {
+        print("Handling next")
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
